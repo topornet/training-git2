c9b4c35d2aeae4b823bf9e5f633fa2c207c67af9
topor.net <topor.net@gmail.com>
2015-04-12 21:41:09 -0300

    Criando tag.

--- src/main/java/br/com/training/vo/App.java
+++ src/main/java/br/com/training/vo/App.java
@@ -3,11 +3,12 @@ package br.com.training.vo;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Training git.");
+		System.out.println("Training git100.");
 		System.out.println("Training git2.");
 		System.out.println("Training git3.");
 		System.out.println("Training git4.");
 		System.out.println("Training git5.");
+		System.out.println("Training tag.");
 		
 	}
 
